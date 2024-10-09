e8e0b2e1f6ef928592d07159add02201eea013fb
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-09 18:58:50 -0300

    fix: userController import was missing .js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import userController from '../controllers/userController';
+import userController from '../controllers/userController.js';
 
 // import loginRequired from '../middlewares/loginRequired'; futuro
 
