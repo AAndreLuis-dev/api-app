b7f2b02bbc67e88c6fd60527b54d41bd34a8b452
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-09 18:57:30 -0300

    feat: install dotenv t0 fix supabase client connection

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
