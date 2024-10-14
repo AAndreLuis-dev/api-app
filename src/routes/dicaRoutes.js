141ae10d950af5fbd0fd68398cd2dedf69cb2c2a
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-14 20:24:28 -0300

    feat: Add new routes for verifying and retrieving dicas by theme

--- src/routes/dicaRoutes.js
+++ src/routes/dicaRoutes.js
@@ -10,6 +10,10 @@ router.post('/dicas', dicaController.create);
 router.put('/dicas/:codigo', dicaController.update);
 router.get('/dicas/:codigo', dicaController.getByCode);
 router.delete('/dicas/:codigo', dicaController.delete);
+router.patch('/dicas/:codigo/verificar', dicaController.verify);
+router.get('/:tema/dicas', dicaController.getAllByTheme);
+router.get('/:tema/dicas/verificadas', dicaController.getAllVerifiedByTheme);
+router.get('/:tema/dicas/nao-verificadas', dicaController.getAllNotVerifiedByTheme);
 
 export default router;
 
