71f11108d9127c91fab79c07944f9a7469d52b9b
AAndreluis-dev <aandreluis.a.a@gmail.com>
2024-10-14 20:40:48 -0300

    add swagger and user documentation

--- src/app.js
+++ src/app.js
@@ -1,24 +1,43 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import userRoutes from './routes/userRoutes.js';
 import dicasRoutes from './routes/dicaRoutes.js';
 /* import ingredientesRoutes from './routes/ingredienteRoutes.js' */
 
+const whiteList = ['http://localhost:3000'];
 
-const whiteList = [
-  'http://localhost:3000/',
-];
 
 const corsOptions = {
     origin: function (origin, callback) {
-      if (whiteList.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Não permitido pelo CORS do site'));
-      }
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Não permitido pelo CORS do site'));
+        }
     }
 };
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API APP',
+            version: '1.0.0',
+            description: 'Documentação da API usada para o desenvolvimento dos aplicativos ecológicos',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/',
+            },
+        ],
+    },
+    apis: ['./src/routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 class App {
     constructor() {
@@ -33,6 +52,7 @@ class App {
         this.app.use(helmet());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
     }
 
     routes() {
