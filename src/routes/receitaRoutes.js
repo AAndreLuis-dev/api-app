ab4eda4c483b31f33ea43c895aee0d3f26ecd1ca
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-10-30 15:58:17 -0300

    Adicionando várias fotos receitas
    Pode adicionar no máximo 5 fotos por receita

--- src/routes/receitaRoutes.js
+++ src/routes/receitaRoutes.js
@@ -1,17 +1,25 @@
-import { Router } from 'express';
-import receitaController from '../controllers/receitaController.js';
-import upload from '../middlewares/uploadMiddleware.js';
+import express from 'express';
+import multer from 'multer';
+import ReceitaController from '../controllers/receitaController.js';
 
-const router = new Router();
+const router = express.Router();
 
-router.get('/receitas', receitaController.getAll);
-router.post('/receitas', upload.single('image'), receitaController.create);
-router.put('/receitas/:codigo', upload.single('image'), receitaController.update);
-router.get('/receitas/:codigo', receitaController.getByCode);
-router.delete('/receitas/:codigo', receitaController.delete);
-router.patch('/receitas/:codigo/verificar', receitaController.verify);
-router.get('/:tema/receitas', receitaController.getAllByTheme);
-router.get('/:tema/receitas/verificadas', receitaController.getAllVerifiedByTheme);
-router.get('/:tema/receitas/nao-verificadas', receitaController.getAllNotVerifiedByTheme);
+// Capacidade de imagem
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    }
+});
+
+router.get('/receitas', ReceitaController.getAll);
+router.post('/receitas', upload.array('files', 10), ReceitaController.create);
+router.put('/receitas/:codigo', upload.array('files', 10), ReceitaController.update);
+router.get('/receitas/:codigo', ReceitaController.getByCode);
+router.delete('/receitas/:codigo', ReceitaController.delete);
+router.patch('/receitas/:codigo/verificar', ReceitaController.verify);
+router.get('/:tema/receitas', ReceitaController.getAllByTheme);
+router.get('/:tema/receitas/verificadas', ReceitaController.getAllVerifiedByTheme);
+router.get('/:tema/receitas/nao-verificadas', ReceitaController.getAllNotVerifiedByTheme);
 
 export default router;
